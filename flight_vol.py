f3ab4cd55603f29a0ca740f0512ff79c34e288fe
Jimmy Kim <jimmykim9001@gmail.com>
2020-12-27 04:11:53 -0800

    added comments to flight_vol.py

--- flight_vol.py
+++ flight_vol.py
@@ -14,11 +14,24 @@ tracon_fns = ['51547', '49612', '29844', '52212', '81669', '22012', '59543']
 tracon_fns = set([f'WEB-Report-{x}.csv' for x in tracon_fns])
 
 def get_first_header_idx(df):
+    """
+    Each volume xlsx file contains a number of extraneous rows at the top.
+    This function calculates the first row which contains the header info
+    @param: df (pd.DataFrame), volume xlsx file
+    """
     for idx in range(df.shape[0]):
         if 'IFR Itinerant' in set(df.iloc[idx, :]):
             return idx
 
 def generate_prefix_header(header_row):
+    """
+    Each volume xlsx file contains two rows that contain information about
+    the columns. The first row is utilized as a prefix (we utilize a tab
+    to separate the prefix and the column name)
+    @param: header_row (pd.Series), the header row
+    @returns: prefix (list of str), contains same number of elements as
+        the header_row. Each string is the prefix for that particular column
+    """
     prefixes = []
     curr_prefix = ''
     for x in header_row:
@@ -32,6 +45,15 @@ def generate_prefix_header(header_row):
     return prefixes
 
 def generate_column_names(col_row, prefix):
+    """
+    This generates the columns for a given volume xlsx file. It utilizes
+    the prefix elements that were extracted and adds the column info.
+    @param: col_row (pd.Series), the row that contains the name of the column
+    @param: prefix (list of str), with same number of elements as col_row
+        each str is the prefix for that particular column
+    @returns: final_cols (list of str), with same number of elements as col_row
+        contains final column names
+    """
     final_cols = []
     enter = '\r\n'
     for idx, elem in enumerate(col_row):
@@ -42,6 +64,12 @@ def generate_column_names(col_row, prefix):
     return final_cols
 
 def load_volume_file(filename):
+    """
+    This loads a volume xlsx file, generates column names, removes extraneous
+    info, and removes duplicated columns
+    @param: filename (str) path to volume file
+    @returns: vol_data (pd.DataFrame) of volume data
+    """
     global tracon_fns
 
     vol_data = pd.read_csv(filename)
@@ -60,6 +88,10 @@ def load_volume_file(filename):
     return vol_data
 
 def load_volume():
+    """
+    This loads in all volume xlsx files and concatenates them together. Must be
+    in datasets folder with WEB-Report in the title.
+    """
     # read in flight volume data
     pds = []
     for x in os.listdir('datasets/'):
@@ -77,12 +109,22 @@ def load_volume():
     return pd.concat(pds, axis = 0, ignore_index = True, sort = True)
 
 def conv_to_int(x):
+    """
+    Helper function to convert string to int, nan when error occurs.
+    @param: x (string) to be converted
+    @returns: int/np.nan of converted x
+    """
     try:
         return int(x)
     except:
         return np.nan
 
 def process_dates(vol_data):
+    """
+    Processes all the dates in the volume data, and removes nans.
+    @param: vol_data (pd.DataFrame) the full volume dataset
+    @returns: vol_data (pd.DataFrame) full volume dataset with cleaned dates
+    """
     vol_data['month'] = vol_data['Date'].str.split("/")\
             .apply(lambda x: conv_to_int(x[0]))
     vol_data['year'] = vol_data['Date'].str.split("/")\
@@ -93,6 +135,11 @@ def process_dates(vol_data):
     return vol_data
     
 def generate_facility_date_dict(vol_data):
+    """
+    This creates a nested dictionary dict[facility] -> dict[date] -> location
+    @param: vol_data (pd.DataFrame) the full volume dataset.
+    @returns: dict[facility] -> dict[date] -> location in vol_data
+    """
     # create dictionary of facility/date -> loc idx (it's a nested dictionary using facility and date
     # consecutively)
     id_to_idx = {}
@@ -104,11 +151,26 @@ def generate_facility_date_dict(vol_data):
     return id_to_idx
 
 def date_to_str(year, month):
+    """
+    Converts year/month pair to a string of month/year with padded month
+    @param: year (int/np.nan) 
+    @param: month (int/np.nan) 
+    @returns month/year
+    """
     month_str = '0' * int(month < 10) + str(month)
     year_str = str(year)
     return f"{month_str}/{year_str}"
 
 def generate_final_row(row, code, date_str, id_to_idx, vol_data):
+    """
+    This combines a row from the incident/accident dataset with the volume data.
+    @param: row (pd.Series), row from incident/accident dataset (airport_month_events.csv)
+    @param: date_str (str) comptued from date_to_str function
+    @param: id_to_idx (nested dict), computed from generate_facility_date_dict function
+    @param: vol_data (pd.DataFrame) the full volume dataset.
+    @returns: final_row (pd.Series), the combined row from incident/accident dataset
+        and the volume dataset. Nans if the volume data could not be found
+    """
     if code in id_to_idx and date_str in id_to_idx[code]:
         final_row = pd.concat([row, vol_data.loc[id_to_idx[code][date_str], :]], axis = 0)
     else:
@@ -116,16 +178,24 @@ def generate_final_row(row, code, date_str, id_to_idx, vol_data):
     return final_row[~final_row.index.duplicated(keep='first')]
 
 def generate_combined_dfs(res, vol_data, id_to_idx):
+    """
+    This generates the combined dataset of the volume data and the incident/accident dataset.
+    @param: res (pd.DataFrame) the incident/accident dataset
+    @param: vol_data (pd.DataFrame) the volume data
+    @param: id_to_idx (nested dict), computed from generate_Facility_date_dict function
+    @returns: all_rows (pd.DataFrame) full combined dataset
+    @returns: cnf (pd.DataFrame) all rows where the code was not found in volume data
+    @returns: dnf (pd.DataFrame) all rows where the code was found but the date was not found
+        in the volume data
+    """
     # generate dataframe. If the incident/accident date is not found in he flight volume data, then
     # it is added to rows_dnf (rows date not found), and if the tracon code is not found in the volume
     # data it is added to rows_cnf (rows code not found). Note nans are used to replace the missing info
     rows, rows_dnf, rows_cnf = [], [], []
     date_nf_ct, code_nf_ct = 0, 0
-    missing_code = 0
     for idx, row in tqdm(res.iterrows(), total = res.shape[0]):
         code = row['airport_code']
         if pd.isna(code):
-            missing_code += 1
             continue
         code = code.strip()
         date_str = date_to_str(row['year'], row['month'])
@@ -155,7 +225,7 @@ def main():
     # combine the two csvs
     res = pd.read_csv('results/airport_month_events.csv', index_col = 0)
 
-    # remove this later
+    # top 50 iata codes
     top_50_iata = set(pd.read_excel('datasets/2010 Busiest Airports wikipedia.xlsx')['IATA'].iloc[1:])
     res = res[res['airport_code'].apply(lambda x: x in top_50_iata)]
 
